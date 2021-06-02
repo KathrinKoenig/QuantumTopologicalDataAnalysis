d36ff073e328685715d7e8402bfd110dda0f3c7e
nephts <nephts@mailbox.org>
2021-06-02 10:38:16 +0200

    commit

--- Code/qtda_modules/qtda_module.py
+++ Code/qtda_modules/qtda_module.py
@@ -242,7 +242,10 @@ class data_filtration:
         '''
         
         if data is not None:
-            self.skeleton = gd.RipsComplex(points = data, max_edge_length = max_edge_length)
+            self.skeleton = gd.RipsComplex(
+                points = data, 
+                max_edge_length = max_edge_length
+                )
         elif distance_matrix is not None:
             self.skeleton = gd.RipsComplex(
                 distance_matrix = distance_matrix, 
@@ -269,6 +272,67 @@ class data_filtration:
         BarCodes = self.Rips_simplex_tree.persistence()
         gd.plot_persistence_diagram(BarCodes, legend=True)
 
+
+
+
+# import scipy
+# import math
+# import qiskit
+
+# def controledU(U, qc, num_qubits, n_vertices):#, k, state_dict):
+#     unit = U
+# #     unit = scipy.linalg.expm(1j*qtda.projected_combinatorial_laplacian(n_vertices, k, state_dict).toarray())
+#     gate = qiskit.extensions.UnitaryGate(unit)
+#     for counting_qubit in range(num_qubits):
+#         qc.append(gate.control(1), [counting_qubit] + list(range(num_qubits,num_qubits+n_vertices)))
+#         unit = unit@unit
+#         gate = gate = qiskit.extensions.UnitaryGate(unit)
+#     return qc
+
+# def qft_dagger(qc, n):
+#     """n-qubit QFTdagger the first n qubits in circ"""
+#     # Don't forget the Swaps!
+#     for qubit in range(n//2):
+#         qc.swap(qubit, n-qubit-1)
+#     for j in range(n):
+#         for m in range(j):
+#             qc.cp(-math.pi/float(2**(j-m)), m, j)
+#         qc.h(j)
+        
+# def qpe_total(num_qubits, n_vertices, unitary): #, k, state_dict):
+#     qc = QuantumCircuit(num_qubits + n_vertices)
+#     for qubit in range(num_qubits):
+#         qc.h(qubit)
+#     controledU(unitary, qc, num_qubits, n_vertices) #, k, state_dict)
+#     # Apply inverse QFT
+#     qft_dagger(qc, num_qubits)
+#     return qc
+
+
+
+
+
+# n_vertices = 4
+# num_qubits = 5
+# k=1
+# shots = 2000
+
+# S0 = [(0,0,0,1),(0,0,1,0), (0,1,0,0),(1,0,0,0)]
+# S1 = [(0,0,1,1),(0,1,1,0),(1,1,0,0),(1,0,0,1),(1,0,1,0)]
+# S2 = []
+# S2 = [(1,0,1,1)]
+# S3 = []
+
+# state_dict = {0: S0, 1: S1, 2: S2, 3: S3}
+# unitary = scipy.linalg.expm(
+#     1j*projected_combinatorial_laplacian(n_vertices, k, state_dict).toarray()
+#     )
+
+# qpe = qpe_total(num_qubits, n_vertices, unitary)
+# qpe.draw('mpl')
+
+
+
 class QTDA_algorithm(QuantumCircuit):
     def __init__(self, num_eval_qubits, top_order, state_dict, name='QTDA'):
 
@@ -284,6 +348,7 @@ class QTDA_algorithm(QuantumCircuit):
             )
         gate = UnitaryGate(unitary)
         qpe = PhaseEstimation(num_eval_qubits, unitary=gate, iqft=None, name='QPE')
+        
         sub_inst = qpe.to_instruction()
         sub_inst.name = '        QPE        '
         
@@ -303,6 +368,57 @@ import time
 from qiskit import IBMQ, Aer, transpile, assemble
 from qiskit import execute
 
+class Q_top_spectra:
+    def __init__(self, state_dict, num_eval_qubits=6, shots=1000):
+        '''
+        data: point data given by (number_data_points x dim_points)-numpy-array
+        distance_matrix: (n x n)-numpy-array describing the pair-wise distances of n data points
+        
+        Either one of them has to be given!
+        '''
+        self.shots = shots
+        self.state_dict = state_dict
+        
+        self.counts = {}
+        for top_order in self.state_dict.keys():
+            print('Topological order: ', top_order)
+            if (len(self.state_dict[top_order])==0):
+                print("calculation terminated because no simplex of dimension %s" %(top_order))
+                break
+            qc = QTDA_algorithm(num_eval_qubits, top_order, self.state_dict)
+            qc.add_register(ClassicalRegister(num_eval_qubits, name="phase"))
+            for q in qc.eval_qubits:
+                qc.measure(q,q)
+            
+            backend = Aer.get_backend('qasm_simulator')
+            job = execute(qc, backend, shots=self.shots)
+            self.counts[top_order] = job.result().get_counts(qc)
+    
+    def get_counts(self):
+        return self.counts
+    
+    def get_spectra(self, chop=None):
+        '''
+        defaul chop: eigenvalues with less then 2% of all shots are regarded as noise (can be adjusted)
+        exception: the eigenspace of eigenvalue 0 is important, even if it is 0-dimensional; hence, this is always included
+        '''
+        if chop == None:
+            chop = self.shots/50   
+        eigenvalue_dict = {}
+        for top_order in self.counts.keys():
+            eigenvalue_dict[top_order] = {}
+            eigenvalue_dict[top_order][0.0] = 0
+            vals = np.fromiter(self.counts[top_order].values(), dtype=float)
+            keys = list(self.counts[top_order].keys())
+            indices = np.where(vals >= chop)
+            new_vals = vals[indices]
+            new_keys = [keys[i] for i in indices[0]]
+            for i in range(len(new_keys)):
+                eigenvalue_dict[top_order][2*np.pi*int(new_keys[i], 2)/int(len(new_keys[i])*'1',2)] = (
+                    new_vals[i] * len(self.state_dict[top_order])/self.shots
+                    )
+        return eigenvalue_dict
+    
 class Q_persistent_top_spectra:
     def __init__(self, data=None, distance_matrix=None, max_dimension=None, max_edge_length=None, num_eval_qubits=6, shots=1000, epsilons=None):
         '''
@@ -335,7 +451,7 @@ class Q_persistent_top_spectra:
             for top_order in self.state_dict[eps].keys():
                 print('Topological order: ', top_order)
                 if (len(self.state_dict[eps][top_order])==0):
-                    print("calculation terminated because no simplex of size ...")
+                    print("calculation terminated because no simplex of dimension %s" %(top_order))
                     break
                 t = time.time()
                 
@@ -376,7 +492,7 @@ class Q_persistent_top_spectra:
                 keys = list(self.counts[eps][top_order].keys())
                 indices = np.where(vals >= chop)
                 new_vals = vals[indices]
-                new_keys = [keys[i] for i in indices[0]]                
+                new_keys = [keys[i] for i in indices[0]]
                 for i in range(len(new_keys)):
                     eigenvalue_dict[eps][top_order][2*np.pi*int(new_keys[i], 2)/int(len(new_keys[i])*'1',2)] = (
                         new_vals[i] * len(self.state_dict[eps][top_order])/self.shots
